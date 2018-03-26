aa5e1531b657992d210f823c1aa744160f43004d
Tom Coleman <tom@thesnail.org>
2018-03-26 14:34:56 +1100

    Added initial task implementation and story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/components/**/*.stories.js'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
