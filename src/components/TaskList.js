8412063e680afe5dfa89ca0573fd1b2715c3e331
Tom Coleman <tom@thesnail.org>
2018-03-26 15:07:37 +1100

    Added stories + default implementation of `TaskList`

---
+++ src/components/TaskList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Task from './Task';
+
+function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+  const events = {
+    onPinTask,
+    onArchiveTask,
+  };
+
+  if (loading) {
+    return <div className="list-items">loading</div>;
+  }
+
+  if (tasks.length === 0) {
+    return <div className="list-items">empty</div>;
+  }
+
+  return (
+    <div className="list-items">
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} {...events} />
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;
---
+++ src/components/TaskList.stories.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import TaskList from './TaskList';
+import { taskData, actionsData } from './Task.stories';
+
+export default {
+  component: TaskList,
+  title: 'TaskList',
+  decorators: [(story) => <div style={{ padding: '3rem' }}>{story()}</div>],
+  excludeStories: /.*Data$/,
+};
+
+export const defaultTasksData = [
+  { ...taskData, id: '1', title: 'Task 1' },
+  { ...taskData, id: '2', title: 'Task 2' },
+  { ...taskData, id: '3', title: 'Task 3' },
+  { ...taskData, id: '4', title: 'Task 4' },
+  { ...taskData, id: '5', title: 'Task 5' },
+  { ...taskData, id: '6', title: 'Task 6' },
+];
+
+export const withPinnedTasksData = [
+  ...defaultTasksData.slice(0, 5),
+  { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+];
+
+export const Default = () => <TaskList tasks={defaultTasksData} {...actionsData} />;
+
+export const WithPinnedTasks = () => <TaskList tasks={withPinnedTasksData} {...actionsData} />;
+
+export const Loading = () => <TaskList loading tasks={[]} {...actionsData} />;
+
+export const Empty = () => <TaskList tasks={[]} {...actionsData} />;
