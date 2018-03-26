0b2efd400ba4f028dc4095a31586bc69ed201b04
Tom Coleman <tom@thesnail.org>
2018-03-26 14:40:24 +1100

    Implemented more complex task

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
   return (
-    <div className="list-item">
-      <input type="text" value={title} readOnly={true} />
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === 'TASK_ARCHIVED'}
+          disabled={true}
+          name="checked"
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+      </label>
+      <div className="title">
+        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+      </div>
+
+      <div className="actions" onClick={event => event.stopPropagation()}>
+        {state !== 'TASK_ARCHIVED' && (
+          <a onClick={() => onPinTask(id)}>
+            <span className={`icon-star`} />
+          </a>
+        )}
+      </div>
     </div>
   );
 }
