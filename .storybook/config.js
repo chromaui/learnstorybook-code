f8a11babc67df740ea9a940a9a593473d04d703c
Tom Coleman <tom@thesnail.org>
2018-03-26 14:33:10 +1100

    Load all *.stories.js files from `src/components`

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,8 +2,10 @@ import { configure } from '@storybook/react';
 
 import '../src/index.css';
 
+const req = require.context('../src/components', true, /\.stories\.js$/);
+
 function loadStories() {
-  require('../src/stories');
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
