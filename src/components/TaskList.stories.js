3d29322928264541322fb620b7d80d3f4ffc5218
Tom Coleman <tom@thesnail.org>
2018-03-27 18:23:25 +1100

    Made into a very basic redux app

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import TaskList from './TaskList';
+import { PureTaskList } from './TaskList';
 import { taskData, actionsData } from './Task.stories';
 
 export default {
-  component: TaskList,
+  component: PureTaskList,
   title: 'TaskList',
-  decorators: [(story) => <div style={{ padding: '3rem' }}>{story()}</div>],
+  decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
   excludeStories: /.*Data$/,
 };
 
@@ -24,10 +24,10 @@ export const withPinnedTasksData = [
   { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
 ];
 
-export const Default = () => <TaskList tasks={defaultTasksData} {...actionsData} />;
+export const Default = () => <PureTaskList tasks={defaultTasksData} {...actionsData} />;
 
-export const WithPinnedTasks = () => <TaskList tasks={withPinnedTasksData} {...actionsData} />;
+export const WithPinnedTasks = () => <PureTaskList tasks={withPinnedTasksData} {...actionsData} />;
 
-export const Loading = () => <TaskList loading tasks={[]} {...actionsData} />;
+export const Loading = () => <PureTaskList loading tasks={[]} {...actionsData} />;
 
-export const Empty = () => <TaskList tasks={[]} {...actionsData} />;
+export const Empty = () => <PureTaskList tasks={[]} {...actionsData} />;
