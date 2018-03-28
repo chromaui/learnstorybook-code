374db5b5a57906f71df85098d40f067633bef5a7
Tom Coleman <tom@thesnail.org>
2018-03-28 13:53:44 +1100

    Added the `@storybook/addon-jest` addon.. Not srue

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withTests } from '@storybook/addon-jest';
 
 import TaskList from './TaskList';
 import { createTask, actions } from './Task.stories';
+import results from '../../jest-test-results.json';
 
 export const defaultTasks = [
   createTask({ state: 'TASK_INBOX' }),
@@ -20,6 +22,7 @@ export const withPinnedTasks = [
 
 storiesOf('TaskList', module)
   .addDecorator(story => <div style={{ background: 'white' }}>{story()}</div>)
+  .addDecorator(withTests({ results })('TaskList'))
   .add('default', () => <TaskList tasks={defaultTasks} {...actions} />)
   .add('w/ pinned tasks', () => <TaskList tasks={withPinnedTasks} {...actions} />)
   .add('loading', () => <TaskList loading {...actions} />)
