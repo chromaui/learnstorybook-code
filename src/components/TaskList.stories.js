ab042a442c7f9707e6d467527109295d17428849
Tom Coleman <tom@thesnail.org>
2018-03-29 12:49:27 +1100

    Revert "Added the `@storybook/addon-jest` addon.. Not srue"
    This reverts commit 374db5b5a57906f71df85098d40f067633bef5a7.

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withTests } from '@storybook/addon-jest';
 
 import { PureTaskList } from './TaskList';
 import { createTask, actions } from './Task.stories';
-import results from '../../jest-test-results.json';
 
 export const defaultTasks = [
   createTask({ state: 'TASK_INBOX' }),
@@ -22,7 +20,6 @@ export const withPinnedTasks = [
 
 storiesOf('TaskList', module)
   .addDecorator(story => <div style={{ background: 'white' }}>{story()}</div>)
-  .addDecorator(withTests({ results })('TaskList'))
   .add('default', () => <PureTaskList tasks={defaultTasks} {...actions} />)
   .add('w/ pinned tasks', () => <PureTaskList tasks={withPinnedTasks} {...actions} />)
   .add('loading', () => <PureTaskList loading {...actions} />)
