8e1e4c8d534612da55418349317141e5f85f3338
Dominic <dom@hichroma.com>
2018-03-30 17:42:23 -0400

    Task updates

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 const alignStyles = {
-  fontSize: '14px',
-  lineHeight: '1.5rem',
-  padding: '0.75em 0.25em',
+  fontSize: "14px",
+  lineHeight: "1.5rem",
+  padding: "0.75em 0.25em"
 };
 
 export default function Task({
-  task: { id, title, url, state, subtitle },
+  task: { id, title, state },
   onArchiveTask,
-  onSnoozeTask,
-  onPinTask,
+  onPinTask
 }) {
   return (
     <div className="list-item">
       <label className="checkbox">
         <input
           type="checkbox"
-          defaultChecked={state === 'TASK_ARCHIVED'}
+          defaultChecked={state === "TASK_ARCHIVED"}
           disabled={true}
           name="checked"
         />
@@ -29,16 +28,8 @@ export default function Task({
         value={title}
         readOnly={true}
         placeholder="Input title"
-        onClick={() => window.open(url, '_new')}
       />
-      {subtitle && <p style={{ flex: 1, color: '#666', ...alignStyles }}>{subtitle}</p>}
-      {state !== 'TASK_SNOOZED' &&
-        state !== 'TASK_ARCHIVED' && (
-          <a style={alignStyles} onClick={() => onSnoozeTask(id)}>
-            <span className="icon-link icon-sync" />
-          </a>
-        )}
-      {state !== 'TASK_PINNED' && (
+      {state !== "TASK_PINNED" && (
         <a style={alignStyles} onClick={() => onPinTask(id)}>
           <span className="icon-link icon-arrow-down" />
         </a>
@@ -51,10 +42,7 @@ Task.propTypes = {
   task: PropTypes.shape({
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    subtitle: PropTypes.string,
-    url: PropTypes.string,
-    state: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired
   }),
-  onSnoozeTask: PropTypes.func,
-  onPinTask: PropTypes.func,
+  onPinTask: PropTypes.func
 };
--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,34 +1,37 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 
-import Task from './Task';
+import Task from "./Task";
 
 export function createTask(attrs) {
   return {
     id: Math.round(Math.random() * 1000000).toString(),
-    title: 'Test Task',
-    subtitle: 'on Test Board',
-    url: 'http://test.url',
-    state: 'TASK_INBOX',
+    title: "Test Task",
+    state: "TASK_INBOX",
     updatedAt: Date.now(),
-    ...attrs,
+    ...attrs
   };
 }
 
 export const actions = {
-  onPinTask: action('onPinTask'),
-  onArchiveTask: action('onArchiveTask'),
-  onUpdateTaskTitle: action('onUpdateTaskTitle'),
+  onPinTask: action("onPinTask"),
+  onArchiveTask: action("onArchiveTask"),
+  onUpdateTaskTitle: action("onUpdateTaskTitle")
 };
 
-storiesOf('Task', module)
+storiesOf("Task", module)
   .addDecorator(story => (
-    <div className="list-items" style={{ background: 'white' }}>
+    <div className="list-items" style={{ background: "white" }}>
       {story()}
     </div>
   ))
-  .add('inbox task', () => <Task task={createTask({ state: 'TASK_INBOX' })} {...actions} />)
-  .add('snoozed task', () => <Task task={createTask({ state: 'TASK_SNOOZED' })} {...actions} />)
-  .add('pinned task', () => <Task task={createTask({ state: 'TASK_PINNED' })} {...actions} />)
-  .add('archived task', () => <Task task={createTask({ state: 'TASK_ARCHIVED' })} {...actions} />);
+  .add("default", () => (
+    <Task task={createTask({ state: "TASK_INBOX" })} {...actions} />
+  ))
+  .add("pinned", () => (
+    <Task task={createTask({ state: "TASK_PINNED" })} {...actions} />
+  ))
+  .add("archived", () => (
+    <Task task={createTask({ state: "TASK_ARCHIVED" })} {...actions} />
+  ));
