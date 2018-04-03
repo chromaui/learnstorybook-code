ea7817a9c0cf8c69b9f7726b09b4a7cfe5265c8e
Tom Coleman <tom@thesnail.org>
2018-04-03 12:08:37 +1000

    Added propTypes to `TaskList` component

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Task from './Task';
 
@@ -55,5 +56,16 @@ function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
     </div>
   );
 }
+
+TaskList.propTypes = {
+  loading: PropTypes.bool,
+  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+  onPinTask: PropTypes.func.isRequired,
+  onArchiveTask: PropTypes.func.isRequired,
+};
+
+TaskList.defaultProps = {
+  loading: false,
+};
 
 export default TaskList;
