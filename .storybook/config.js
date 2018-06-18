0fb7605320fd091057bd29b60f4c3a345f6409a9
Tom Coleman <tom@thesnail.org>
2018-06-18 15:35:05 +1000

    Added very basic task implementation and story

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
+import Vue from 'vue';
 import { configure } from '@storybook/vue';
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
+import '../src/index.css';
+
+const req = require.context('../src', true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
---
+++ src/components/Task.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="list-item">
+    <input type="text" :readonly="true" :value="this.task.title" />
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'task',
+  props: {
+    task: Object,
+    required: true,
+  },
+};
+</script>
