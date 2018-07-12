271f39d447ea97485a373f8994bf3d870015221f
Kyle Holmberg <inbox@kylemh.com>
2018-07-11 23:54:13 -0700

    Add and integrate knobs addon Fixed component implementation Added missing story Conflicts being resolved for 6.0

--- src/components/Task.js
+++ src/components/Task.js
@@ -14,7 +14,7 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
       <div className="title">
-        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+        <input type="text" value={title} readOnly={true} placeholder="Input title" style={{ textOverflow: 'ellipsis' }} />
       </div>
 
       <div className="actions" onClick={event => event.stopPropagation()}>
