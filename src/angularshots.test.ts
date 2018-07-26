1a1491999733316a3e18b66123f9c29d7379c473
Carlos Vega <clmvega@gmail.com>
2018-07-26 12:37:43 -0600

    Added jest and storyshots

---
+++ src/angularshots.test.ts
@@ -0,0 +1,10 @@
+import * as path from 'path';
+import initStoryshots, {
+  multiSnapshotWithOptions,
+} from '@storybook/addon-storyshots';
+
+initStoryshots({
+  framework: 'angular',
+  configPath: path.join(__dirname, '../.storybook'),
+  test: multiSnapshotWithOptions(),
+});
