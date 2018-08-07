a10d3f5f61ec1aa223892ff82c56aa9b4887294e
Tom Coleman <tom@thesnail.org>
2018-08-07 12:26:28 +1000

    Implemented more complex task

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,6 +1,22 @@
 <template>
-  <div class="list-item">
-    <input type="text" :readonly="true" :value="this.task.title" />
+  <div :class="taskClass">
+    <label class="checkbox">
+      <input
+        type="checkbox"
+        :checked="isChecked"
+        :disabled="true"
+        name="checked"
+      />
+      <span class="checkbox-custom" @click="$emit('archiveTask', task.id)"/>
+    </label>
+    <div class="title">
+      <input type="text" :readonly="true" :value="this.task.title" placeholder="Input title" />
+    </div>
+    <div class="actions">
+      <a @click="$emit('pinTask', task.id)" v-if="!isChecked">
+        <span class="icon-star"/>
+      </a>
+    </div>
   </div>  
 </template>
 
@@ -10,6 +26,14 @@ export default {
   props: {
     task: Object,
     required: true
+  },
+  computed: {
+    taskClass() {
+      return `list-item ${this.task.state}`;
+    },
+    isChecked() {
+      return this.task.state === "TASK_ARCHIVED";
+    }
   }
 };
 </script>
