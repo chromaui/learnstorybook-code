8db511e7204027e7f40f27928edc116b162cd7de
Tom Coleman <tom@percolatestudio.com>
2019-03-05 13:28:16 +1100

    Added a Jest test of the ordering of the

---
+++ src/components/TaskList.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TaskList from './TaskList';
+import { withPinnedTasks } from './TaskList.stories';
+
+it('renders pinned tasks at the start of the list', () => {
+  const div = document.createElement('div');
+  const events = { onPinTask: jest.fn(), onArchiveTask: jest.fn() };
+  ReactDOM.render(<TaskList tasks={withPinnedTasks} {...events} />, div);
+
+  // We expect the task titled "Task 6 (pinned)" to be rendered first, not at the end
+  const lastTaskInput = div.querySelector('.list-item:nth-child(1) input[value="Task 6 (pinned)"]');
+  expect(lastTaskInput).not.toBe(null);
+
+  ReactDOM.unmountComponentAtNode(div);
+});
