fa036312f370586c90c66671b52f0058f1c7a279
Tom Coleman <tom@thesnail.org>
2020-05-18 15:43:18 +1000

    Actually show the designs Conflicts are solved for 6.0

--- .storybook/design-addon/register.js
+++ .storybook/design-addon/register.js
@@ -1,31 +1,52 @@
 import React, { Fragment } from 'react';
 import { AddonPanel } from '@storybook/components';
-import { useParameter } from '@storybook/api';
+import { useParameter, useStorybookState } from '@storybook/api';
 import { addons, types } from '@storybook/addons';
+import { styled } from '@storybook/theming';
+
+const getUrl = (input) => {
+  return typeof input === 'string' ? input : input.url;
+};
+
+const Iframe = styled.iframe({
+  width: '100%',
+  height: '100%',
+  border: '0 none',
+});
+const Img = styled.img({
+  width: '100%',
+  height: '100%',
+  border: '0 none',
+  objectFit: 'contain',
+});
+
+const Asset = ({ url }) => {
+  if (!url) {
+    return null;
+  }
+  if (url.match(/\.(png|gif|jpeg|tiff|svg|anpg|webp)/)) {
+    // do image viewer
+    return <Img alt="" src={url} />;
+  }
+
+  return <Iframe title={url} src={url} />;
+};
 
 const Content = () => {
-  const results = useParameter('assets', []); // story's parameter being retrieved here
+  // story's parameter being retrieved here
+  const results = useParameter('assets', []);
+  // the id of story retrieved from Storybook global state
+  const { storyId } = useStorybookState();
+
+  if (results.length === 0) {
+    return null;
+  }
+
+  const url = getUrl(results[0]).replace('{id}', storyId);
+
   return (
     <Fragment>
-      {results.length ? (
-        <ol>
-          {results.map((i) => (
-            <li>{i}</li>
-          ))}
-        </ol>
-      ) : null}
+      <Asset url={url} />
     </Fragment>
   );
 };
-
-addons.register('my/design-addon', () => {
-  addons.add('design-addon/panel', {
-    title: 'assets',
-    type: types.PANEL,
-    render: ({ active, key }) => (
-      <AddonPanel active={active} key={key}>
-        <Content />
-      </AddonPanel>
-    ),
-  });
-});
