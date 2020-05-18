829068d5f9ff5d5bfec44a383a66afe11982af11
Tom Coleman <tom@thesnail.org>
2020-05-18 15:40:10 +1000

    Use parameter to send assets to addon Resolved conflicts for 6.0

--- .storybook/design-addon/register.js
+++ .storybook/design-addon/register.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { AddonPanel } from '@storybook/components';
+import { useParameter } from '@storybook/api';
 import { addons, types } from '@storybook/addons';
+
+const Content = () => {
+  const results = useParameter('assets', []); // story's parameter being retrieved here
+  return (
+    <Fragment>
+      {results.length ? (
+        <ol>
+          {results.map((i) => (
+            <li>{i}</li>
+          ))}
+        </ol>
+      ) : null}
+    </Fragment>
+  );
+};
 
 addons.register('my/design-addon', () => {
   addons.add('design-addon/panel', {
@@ -8,7 +24,7 @@ addons.register('my/design-addon', () => {
     type: types.PANEL,
     render: ({ active, key }) => (
       <AddonPanel active={active} key={key}>
-        implement
+        <Content />
       </AddonPanel>
     ),
   });
