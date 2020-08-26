433867f1c869047aefbf2c94cfeab5fb32b87c70
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2020-08-26 23:18:52 +0100

    Fixed the wrong story file

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,35 +1,46 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { withKnobs, object } from '@storybook/addon-knobs/react';
+import React from "react";
 
-import Task from './Task';
+import Task from "./Task";
 
 export default {
   component: Task,
-  title: 'Task',
-  decorators: [withKnobs],
-  parameters: { assets: ['designs/items.png'] },
-  // Our exports that end in "Data" are not stories.
-  excludeStories: /.*Data$/,
+  title: "Task",
 };
 
-export const taskData = {
-  id: '1',
-  title: 'Test Task',
-  state: 'TASK_INBOX',
-  updatedAt: new Date(2018, 0, 1, 9, 0),
+const Template = (args) => <Task {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: "1",
+    title: "Test Task",
+    state: "TASK_INBOX",
+    updatedAt: new Date(2018, 0, 1, 9, 0),
+  },
 };
 
-export const actionsData = {
-  onPinTask: action('onPinTask'),
-  onArchiveTask: action('onArchiveTask'),
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    state: "TASK_PINNED",
+  },
 };
 
-export const Default = () => {
-  return <Task task={object('task', { ...taskData })} {...actionsData} />;
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: "TASK_ARCHIVED",
+  },
 };
-export const Pinned = () => <Task task={{ ...taskData, state: 'TASK_PINNED' }} {...actionsData} />;
 
-export const Archived = () => (
-  <Task task={{ ...taskData, state: 'TASK_ARCHIVED' }} {...actionsData} />
-);
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
+export const LongTitle = Template.bind({});
+LongTitle.args = {
+  task: {
+    ...Default.args.task,
+    title: longTitleString,
+  }
+};
