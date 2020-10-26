48d91031dd3433b3faa69b1e1c284901285acc47
temoji <teeeemoji@163.com>
2020-10-26 23:31:53 +0800

    Update storybook version to 6.x

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,23 @@
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const path = require('path');
+const path = require("path")
+    // Export a function. Accept the base config as the only param.
+module.exports = async({ config, mode }) => {
+    // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
 
-module.exports = (storybookBaseConfig, configType) => {
-  storybookBaseConfig.plugins.push(new VueLoaderPlugin());
-  storybookBaseConfig.module.rules.push({
-    test: /\.css$/,
-    use: ['vue-style-loader', 'css-loader'],
-  });
-  storybookBaseConfig.resolve = {
-    extensions: ['.js', '.vue', '.json'],
-    alias: {
-      vue$: 'vue/dist/vue.esm.js',
-      '@': path.join(__dirname, '..', 'src'),
-    },
-  };
-  return storybookBaseConfig;
-};
+    // Make whatever fine-grained changes you need
+
+    /**
+     * Using .less files with (Vue &) Storybook
+     * https://notestack.io/public/using-less-files-with-vue--storybook-/ad341f84-1e60-4c3d-9588-ebd3286e3551
+     */
+    config.module.rules.push({
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+        include: path.resolve(__dirname, "../")
+    })
+
+    config.resolve.alias["@"] = path.resolve(__dirname, "../src")
+        // Return the altered config
+    return config
+}
