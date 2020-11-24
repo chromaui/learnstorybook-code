d2fca1f9befe258d54dd4f3b5d1ecd51d109c249
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2020-11-24 18:53:22 +0000

    Made into a very basic redux app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './lib/redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TaskList from './components/TaskList';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <TaskList />
+      </Provider>
+    );
+  }
 }
 
 export default App;
