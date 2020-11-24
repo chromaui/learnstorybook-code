31664a01a0c27f7d3cdb302bcc815ac349ded3e0
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2020-11-24 18:59:50 +0000

    Added `InboxScreen` component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './lib/redux';
 
-import TaskList from './components/TaskList';
+import InboxScreen from './components/InboxScreen';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <TaskList />
+        <InboxScreen />
       </Provider>
     );
   }
