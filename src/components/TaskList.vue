556bb58f377f0d0b9f45a26a73c26c4b75336d4c
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2020-12-18 12:32:41 +0000

    Added stories + default implementation of `TaskList`

---
+++ src/components/TaskList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="list-items">
+    <template v-if="loading">
+      loading
+    </template>
+    <template v-else-if="isEmpty">
+      empty
+    </template>
+    <template v-else>
+      <Task
+        v-for="task in tasks"
+        :key="task.id"
+        :task="task"
+        v-on="$listeners"
+      />
+    </template>
+  </div>
+</template>
+
+<script>
+import Task from "./Task";
+export default {
+  name: "TaskList",
+  components: { Task },
+  props: {
+    tasks: { type: Array, required: true, default: () => [] },
+    loading: { type: Boolean, default: false }
+  },
+  computed: {
+    isEmpty() {
+      return this.tasks.length === 0;
+    }
+  }
+};
+</script>
