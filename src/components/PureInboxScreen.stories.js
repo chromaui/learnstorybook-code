99d3d650dffde2b5e149eaeff80606dcd84618c1
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2020-12-18 13:09:31 +0000

    Use a redux-mock decorator in `InboxScreen` stories

--- src/components/PureInboxScreen.stories.js
+++ src/components/PureInboxScreen.stories.js
@@ -1,14 +1,36 @@
+import Vue from "vue";
+import Vuex from "vuex";
 import PureInboxScreen from "./PureInboxScreen.vue";
+import { action } from "@storybook/addon-actions";
+import * as TaskListStories from "./PureTaskList.stories";
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    tasks: TaskListStories.Default.args.tasks
+  },
+  actions: {
+    pinTask(context, id) {
+      action("pin-task")(id);
+    },
+    archiveTask(context, id) {
+      action("archive-task")(id);
+    }
+  }
+});
 
 export default {
   title: "PureInboxScreen",
-  component: PureInboxScreen
+  component: PureInboxScreen,
+  excludeStories: /.*store$/
 };
 
 const Template = (args, { argTypes }) => ({
   components: { PureInboxScreen },
   props: Object.keys(argTypes),
-  template: '<PureInboxScreen v-bind="$props" />'
+  template: '<PureInboxScreen v-bind="$props" />',
+  store
 });
 
 export const Default = Template.bind({});
