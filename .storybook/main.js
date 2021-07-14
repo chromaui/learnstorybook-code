0b314e4b1119434c269259ee36c26c2fd60a43b8
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-07-14 20:42:29 +0100

    Added initial task implementation and story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,8 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/components/**/*.stories.js"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+    "@storybook/preset-create-react-app",
+  ],
+};
