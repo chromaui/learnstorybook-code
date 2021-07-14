09f3ff3d80e98cfcec2d14caa6e2a07bfe833450
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-07-14 21:32:02 +0100

    Add and integrate controls story

--- src/components/Task.js
+++ src/components/Task.js
@@ -23,6 +23,7 @@ export default function Task({
           value={title}
           readOnly={true}
           placeholder="Input title"
+          style={{ textOverflow: "ellipsis" }}
         />
       </div>
 
--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -34,3 +34,13 @@ Archived.args = {
     state: "TASK_ARCHIVED",
   },
 };
+
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
+export const LongTitle = Template.bind({});
+LongTitle.args = {
+  task: {
+    ...Default.args.task,
+    title: longTitleString,
+  },
+};
