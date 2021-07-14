2a9523e48861ad7840a49ea6b334a2b42d9512aa
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-07-14 23:02:17 +0100

    Added initial task implementation and story

---
+++ src/components/Task.stories.js
@@ -0,0 +1,53 @@
+import Task from "./Task.vue";
+
+import { action } from "@storybook/addon-actions";
+
+export default {
+  component: Task,
+  //👇 Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/,
+  title: "Task",
+  //👇 Our events will be mapped in Storybook UI
+  argTypes: {
+    onPinTask: {},
+    onArchiveTask: {},
+  },
+};
+
+export const actionsData = {
+  onPinTask: action("pin-task"),
+  onArchiveTask: action("archive-task"),
+};
+
+const Template = (args) => ({
+  components: { Task },
+  setup() {
+    return { args, ...actionsData };
+  },
+  template: '<Task v-bind="args" />',
+});
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: "1",
+    title: "Test Task",
+    state: "TASK_INBOX",
+    updatedAt: new Date(2018, 0, 1, 9, 0),
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    state: "TASK_PINNED",
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: "TASK_ARCHIVED",
+  },
+};
---
+++ src/components/Task.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="list-item">
+    <input type="text" readonly :value="task.title" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: {
+      type: Object,
+      required: true,
+      default: () => ({ id: "", state: "", title: "" }),
+      validator: (task) => ["id", "state", "title"].every((key) => key in task),
+    },
+  },
+};
+</script>
