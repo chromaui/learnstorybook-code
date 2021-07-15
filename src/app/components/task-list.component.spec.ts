e81ad711180693e8309e94cef42c0254a6e0fdb1
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-07-15 01:39:32 +0100

    Added a Unit test of the ordering of the Tasks

---
+++ src/app/components/task-list.component.spec.ts
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/angular';
+
+import { TaskListComponent } from './task-list.component';
+import { TaskComponent } from './task.component';
+
+//👇 Our story imported here
+import { WithPinnedTasks } from './task-list.stories';
+
+describe('TaskList component', () => {
+  it('renders pinned tasks at the start of the list', async () => {
+    const mockedActions = jest.fn();
+    const tree = await render(TaskListComponent, {
+      declarations: [TaskComponent],
+      componentProperties: {
+        ...WithPinnedTasks.args,
+        onPinTask: {
+          emit: mockedActions,
+        } as any,
+        onArchiveTask: {
+          emit: mockedActions,
+        } as any,
+      },
+    });
+    const component = tree.fixture.componentInstance;
+    expect(component.tasksInOrder[0].id).toBe('6');
+  });
+});
