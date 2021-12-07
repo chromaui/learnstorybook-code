e9d7c3e37e5521c515ec0427c81ceb8396ada402
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-12-07 22:38:50 +0000

    Added initial task implementation and story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.js"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import "../src/index.css";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +8,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
