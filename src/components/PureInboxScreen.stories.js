ab55489806f36d9e12dfb1794d39d214158fe559
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-12-07 21:36:25 +0000

    Use a redux-mock decorator in InboxScreen stories

--- src/components/PureInboxScreen.stories.js
+++ src/components/PureInboxScreen.stories.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 import { PureInboxScreen } from "./InboxScreen";
 
+import * as TaskListStories from "./TaskList.stories";
+
+// A super-simple mock of a redux store
+const Mockstore = configureStore({
+  reducer: {
+    tasks: createSlice({
+      name: "tasks",
+      initialState: TaskListStories.Default.args.tasks,
+      reducers: {
+        updateTaskState: (state, action) => {
+          const { id, newTaskState } = action.payload;
+          const task = state.findIndex((task) => task.id === id);
+          if (task >= 0) {
+            state[task].state = newTaskState;
+          }
+        },
+      },
+    }).reducer,
+  },
+});
+
 export default {
   component: PureInboxScreen,
+  decorators: [(story) => <Provider store={Mockstore}>{story()}</Provider>],
   title: "PureInboxScreen",
 };
 
