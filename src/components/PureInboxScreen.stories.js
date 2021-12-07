05aa2ef7642093f14e7bf981e7424666bd2f924a
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-12-07 21:41:50 +0000

    Added Interactive Story

--- src/components/PureInboxScreen.stories.js
+++ src/components/PureInboxScreen.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { fireEvent, within } from "@storybook/testing-library";
 
 import { PureInboxScreen } from "./InboxScreen";
 
@@ -38,4 +39,13 @@ export const Default = Template.bind({});
 export const Error = Template.bind({});
 Error.args = {
   error: "Something",
+};
+
+export const WithInteractions = Template.bind({});
+WithInteractions.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Simulates pinning the first task
+  await fireEvent.click(canvas.getByLabelText("pinTask-1"));
+  // Simulates pinning the third task
+  await fireEvent.click(canvas.getByLabelText("pinTask-3"));
 };
