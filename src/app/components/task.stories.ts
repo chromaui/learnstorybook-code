572aa4f30f4619642045055b609241d69b9dcae2
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-12-08 00:36:40 +0000

    Added initial task implementation and story

---
+++ src/app/components/task.stories.ts
@@ -0,0 +1,58 @@
+import { moduleMetadata, Meta, Story } from '@storybook/angular';
+
+import { CommonModule } from '@angular/common';
+
+import { action } from '@storybook/addon-actions';
+
+import { TaskComponent } from './task.component';
+
+export default {
+  component: TaskComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [TaskComponent],
+      imports: [CommonModule],
+    }),
+  ],
+  title: 'Task',
+  excludeStories: /.*Data$/,
+} as Meta;
+
+export const actionsData = {
+  onPinTask: action('onPinTask'),
+  onArchiveTask: action('onArchiveTask'),
+};
+
+const Template: Story = (args) => ({
+  props: {
+    ...args,
+    onPinTask: actionsData.onPinTask,
+    onArchiveTask: actionsData.onArchiveTask,
+  },
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: '1',
+    title: 'Test Task',
+    state: 'TASK_INBOX',
+    updatedAt: new Date(2021, 0, 1, 9, 0),
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_PINNED',
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_ARCHIVED',
+  },
+};
