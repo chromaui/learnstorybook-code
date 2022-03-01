9e28bdc9537590fbce2addb6656872cb4bc4657f
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-03-01 17:39:06 +0000

    Added Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
+  ],
+  features: {
+    postcss: false,
+  },
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack4",
+  },
+};
