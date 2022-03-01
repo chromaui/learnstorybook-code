84beca47945df4ac534ef90eeebfcbafcfa01e3a
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-03-01 18:17:05 +0000

    Added InboxScreen component

---
+++ src/components/InboxScreen.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTasks } from "../lib/store";
+import TaskList from "./TaskList";
+
+export default function InboxScreen() {
+  const dispatch = useDispatch();
+  // We're retrieving the error field from our updated store
+  const { error } = useSelector((state) => state.taskbox);
+  // The useEffect triggers the data fetching when the component is mounted
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, []);
+
+  if (error) {
+    return (
+      <div className="page lists-show">
+        <div className="wrapper-message">
+          <span className="icon-face-sad" />
+          <div className="title-message">Oh no!</div>
+          <div className="subtitle-message">Something went wrong</div>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="page lists-show">
+      <nav>
+        <h1 className="title-page">
+          <span className="title-wrapper">Taskbox</span>
+        </h1>
+      </nav>
+      <TaskList />
+    </div>
+  );
+}
