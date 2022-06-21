754421299683d46062b98334376c43acead14093
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-06-21 22:06:16 +0100

    Added Interactive Story

--- src/app/components/pure-inbox-screen.stories.ts
+++ src/app/components/pure-inbox-screen.stories.ts
@@ -1,5 +1,7 @@
 import { moduleMetadata, Meta, Story } from '@storybook/angular';
 
+import { fireEvent, within } from '@storybook/testing-library';
+
 import { CommonModule } from '@angular/common';
 
 import { PureInboxScreenComponent } from './pure-inbox-screen.component';
@@ -30,4 +32,12 @@ export const Default = Template.bind({});
 export const Error = Template.bind({});
 Error.args = {
   error: true,
+};
+export const WithInteractions = Template.bind({});
+WithInteractions.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Simulates pinning the first task
+  await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+  // Simulates pinning the third task
+  await fireEvent.click(canvas.getByLabelText('pinTask-3'));
 };
