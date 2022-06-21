7350241ab0f86743113b1e49f61e9dcc252122bf
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-06-21 19:05:54 +0100

    Implemented more complex task

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,10 +6,43 @@ export default function Task({
   onPinTask,
 }) {
   return (
-    <div className="list-item">
-      <label htmlFor="title" aria-label={title}>
-        <input type="text" value={title} readOnly={true} name="title" />
+    <div className={`list-item ${state}`}>
+      <label
+        htmlFor="checked"
+        aria-label={`archiveTask-${id}`}
+        className="checkbox"
+      >
+        <input
+          type="checkbox"
+          disabled={true}
+          name="checked"
+          id={`archiveTask-${id}`}
+          checked={state === "TASK_ARCHIVED"}
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
+
+      <label htmlFor="title" aria-label={title} className="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          name="title"
+          placeholder="Input title"
+        />
+      </label>
+
+      {state !== "TASK_ARCHIVED" && (
+        <button
+          className="pin-button"
+          onClick={() => onPinTask(id)}
+          id={`pinTask-${id}`}
+          aria-label={`pinTask-${id}`}
+          key={`pinTask-${id}`}
+        >
+          <span className={`icon-star`} />
+        </button>
+      )}
     </div>
   );
 }
