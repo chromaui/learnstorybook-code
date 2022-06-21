9411971488279737145341824e3a9ec3e42dff70
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-06-21 21:38:05 +0100

    Added A11y addon and fixed issues

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: "@storybook/angular",
   core: {
