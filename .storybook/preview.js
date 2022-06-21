e72ef656a9277040102f2fa0804b184683c8b82f
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-06-21 19:26:12 +0100

    Use MSW with Storybook in InboxScreen stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,15 @@
 import "../src/index.css";
 
+// Registers the msw addon
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+// Initialize MSW
+initialize();
+
+// Provide the MSW addon decorator globally
+export const decorators = [mswDecorator];
+
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
