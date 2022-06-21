8e6d35eefe6bf3ce6fc30d327539cfe6f1297910
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-06-21 21:44:54 +0100

    Filled out implementation of TaskList

--- src/app/components/task-list.component.ts
+++ src/app/components/task-list.component.ts
@@ -1,37 +1,62 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
 import { Task } from '../models/task.model';
 
 @Component({
   selector: 'app-task-list',
   template: `
     <div class="list-items">
-      <div *ngIf="loading">loading</div>
-      <div *ngIf="!loading && tasks.length === 0">empty</div>
       <app-task
-        *ngFor="let task of tasks"
+        *ngFor="let task of tasksInOrder"
         [task]="task"
         (onArchiveTask)="onArchiveTask.emit($event)"
         (onPinTask)="onPinTask.emit($event)"
       >
       </app-task>
+      <div
+        *ngIf="tasksInOrder.length === 0 && !loading"
+        class="wrapper-message"
+      >
+        <span class="icon-check"></span>
+        <p class="title-message">You have no tasks</p>
+        <p class="subtitle-message">Sit back and relax</p>
+      </div>
+      <div *ngIf="loading">
+        <div *ngFor="let i of [1, 2, 3, 4, 5, 6]" class="loading-item">
+          <span class="glow-checkbox"></span>
+          <span class="glow-text">
+            <span>Loading</span> <span>cool</span> <span>state</span>
+          </span>
+        </div>
+      </div>
     </div>
   `,
 })
 export class TaskListComponent {
-  /** The list of tasks */
-  @Input() tasks: Task[] = [];
+  /**
+   * @ignore
+   * Component property to define ordering of tasks
+   */
+  tasksInOrder: Task[] = [];
 
-  /** Checks if it's in loading state */
   @Input() loading = false;
 
-  /** Event to change the task to pinned */
   // tslint:disable-next-line: no-output-on-prefix
-  @Output()
-  onPinTask = new EventEmitter<Event>();
+  @Output() onPinTask: EventEmitter<any> = new EventEmitter();
 
-  /** Event to change the task to archived */
   // tslint:disable-next-line: no-output-on-prefix
-  @Output()
-  onArchiveTask = new EventEmitter<Event>();
+  @Output() onArchiveTask: EventEmitter<any> = new EventEmitter();
+
+  @Input()
+  set tasks(arr: Task[]) {
+    const initialTasks = [
+      ...arr.filter((t) => t.state === 'TASK_PINNED'),
+      ...arr.filter((t) => t.state !== 'TASK_PINNED'),
+    ];
+    const filteredTasks = initialTasks.filter(
+      (t) => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'
+    );
+    this.tasksInOrder = filteredTasks.filter(
+      (t) => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'
+    );
+  }
 }
