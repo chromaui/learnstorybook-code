cffe2b91fee43f4f3521df333083132b5faf2707
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-10-04 11:34:20 +0100

    Added Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/vue3",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  features: {
+    interactionsDebugger: true,
+  },
+};
