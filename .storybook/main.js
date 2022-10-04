6cb2cb5161a1762f0011137c9fcda5491fdb9a2a
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-10-04 15:15:19 +0100

    Added A11y addon and fixed issues

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: "@storybook/vue3",
   core: {
