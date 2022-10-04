431a9690cea8871829b664ecbd98a09b1a3cf1b6
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-10-04 23:36:27 +0100

    Added Interactive Story

--- src/components/PureInboxScreen.stories.js
+++ src/components/PureInboxScreen.stories.js
@@ -1,5 +1,7 @@
 import { app } from "@storybook/vue3";
 
+import { fireEvent, within } from "@storybook/testing-library";
+
 import { createPinia } from "pinia";
 
 app.use(createPinia());
@@ -25,3 +27,12 @@ export const Default = Template.bind({});
 
 export const Error = Template.bind({});
 Error.args = { error: true };
+
+export const WithInteractions = Template.bind({});
+WithInteractions.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Simulates pinning the first task
+  await fireEvent.click(canvas.getByLabelText("pinTask-1"));
+  // Simulates pinning the third task
+  await fireEvent.click(canvas.getByLabelText("pinTask-3"));
+};
