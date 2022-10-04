f314b6ea610614e830e0d9f9af91f2e50737607d
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-10-04 14:57:36 +0100

    Added initial task implementation and story

---
+++ src/components/Task.stories.js
@@ -0,0 +1,52 @@
+import Task from "./Task.vue";
+
+import { action } from "@storybook/addon-actions";
+
+export default {
+  component: Task,
+  //👇 Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/,
+  title: "Task",
+  //👇 Our events will be mapped in Storybook UI
+  argTypes: {
+    onPinTask: {},
+    onArchiveTask: {},
+  },
+};
+
+export const actionsData = {
+  onPinTask: action("pin-task"),
+  onArchiveTask: action("archive-task"),
+};
+
+const Template = (args) => ({
+  components: { Task },
+  setup() {
+    return { args, ...actionsData };
+  },
+  template: '<Task v-bind="args" />',
+});
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: "1",
+    title: "Test Task",
+    state: "TASK_INBOX",
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    state: "TASK_PINNED",
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: "TASK_ARCHIVED",
+  },
+};
