a6da696af60da697fa7064a10f7db7198e28c0ac
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-10-04 22:59:15 +0100

    Added InboxScreen component

---
+++ src/components/InboxScreen.vue
@@ -0,0 +1,26 @@
+<template>
+  <PureInboxScreen :error="isError" />
+</template>
+
+<script>
+import PureInboxScreen from './PureInboxScreen';
+
+import { computed } from 'vue';
+
+import { useTaskStore } from '../store';
+
+export default {
+  name: 'InboxScreen',
+  components: { PureInboxScreen },
+  setup() {
+    //👇 Creates a store instance
+    const store = useTaskStore();
+
+    //👇 Retrieves the error from the store's state
+    const isError = computed(() => store.status==='error');
+    return {
+      isError,
+    };
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+
+createApp(App).use(createPinia()).mount("#app");
