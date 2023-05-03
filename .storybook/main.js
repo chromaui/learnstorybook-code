f32289f62932618e1e451d91dfdb3b27a8a7e3f2
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-05-03 16:29:28 +0100

    Added initial task implementation and story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.@(js|jsx)"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
