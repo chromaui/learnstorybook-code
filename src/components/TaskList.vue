77762bd4e47923769ee12c44a6d6865071b06d95
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-05-04 18:18:42 +0100

    Made into a simple Pinia app

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,67 +1,32 @@
 <template>
-  <div class="list-items">
-    <template v-if="loading">
-      <div v-for="n in 6" :key="n" class="loading-item">
-        <span class="glow-checkbox" />
-        <span class="glow-text"> <span>Loading</span> <span>cool</span> <span>state</span> </span>
-      </div>
-    </template>
-
-    <div v-else-if="isEmpty" class="list-items">
-      <div class="wrapper-message">
-        <span class="icon-check" />
-        <p class="title-message">You have no tasks</p>
-        <p class="subtitle-message">Sit back and relax</p>
-      </div>
-    </div>
-
-    <template v-else>
-      <Task
-        v-for="task in tasksInOrder"
-        :key="task.id"
-        :task="task"
-        @archive-task="onArchiveTask"
-        @pin-task="onPinTask"
-      />
-    </template>
-  </div>
+  <PureTaskList :tasks="tasks" @archive-task="archiveTask" @pin-task="pinTask" />
 </template>
 
 <script>
-import Task from './Task.vue';
-import { reactive, computed } from 'vue';
+import PureTaskList from './PureTaskList.vue';
+
+import { computed } from 'vue';
+
+import { useTaskStore } from '../store';
 
 export default {
+  components: { PureTaskList },
   name: 'TaskList',
-  components: { Task },
-  props: {
-    tasks: { type: Array, required: true, default: () => [] },
-    loading: { type: Boolean, default: false },
-  },
-  emits: ['archive-task', 'pin-task'],
+  setup() {
+    //👇 Creates a store instance
+    const store = useTaskStore();
 
-  setup(props, { emit }) {
-    props = reactive(props);
+    //👇 Retrieves the tasks from the store's state auxiliary getter function
+    const tasks = computed(() => store.getFilteredTasks);
+
+    //👇 Dispatches the actions back to the store
+    const archiveTask = (task) => store.archiveTask(task);
+    const pinTask = (task) => store.pinTask(task);
+
     return {
-      isEmpty: computed(() => props.tasks.length === 0),
-      tasksInOrder: computed(() => {
-        return [
-          ...props.tasks.filter((t) => t.state === 'TASK_PINNED'),
-          ...props.tasks.filter((t) => t.state !== 'TASK_PINNED'),
-        ];
-      }),
-      /**
-       * Event handler for archiving tasks
-       */
-      onArchiveTask(taskId) {
-        emit('archive-task', taskId);
-      },
-      /**
-       * Event handler for pinning tasks
-       */
-      onPinTask(taskId) {
-        emit('pin-task', taskId);
-      },
+      tasks,
+      archiveTask,
+      pinTask,
     };
   },
 };
