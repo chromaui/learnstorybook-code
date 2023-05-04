95eea8187db4af29f9a0388456db2579ffe58103
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-05-04 17:57:19 +0100

    Filled out implementation of TaskList

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="list-items">
-    <template v-if="loading"> loading </template>
-    <template v-else-if="isEmpty"> empty </template>
+    <template v-if="loading">
+      <div v-for="n in 6" :key="n" class="loading-item">
+        <span class="glow-checkbox" />
+        <span class="glow-text"> <span>Loading</span> <span>cool</span> <span>state</span> </span>
+      </div>
+    </template>
+
+    <div v-else-if="isEmpty" class="list-items">
+      <div class="wrapper-message">
+        <span class="icon-check" />
+        <p class="title-message">You have no tasks</p>
+        <p class="subtitle-message">Sit back and relax</p>
+      </div>
+    </div>
+
     <template v-else>
       <Task
-        v-for="task in tasks"
+        v-for="task in tasksInOrder"
         :key="task.id"
         :task="task"
         @archive-task="onArchiveTask"
@@ -31,6 +44,12 @@ export default {
     props = reactive(props);
     return {
       isEmpty: computed(() => props.tasks.length === 0),
+      tasksInOrder: computed(() => {
+        return [
+          ...props.tasks.filter((t) => t.state === 'TASK_PINNED'),
+          ...props.tasks.filter((t) => t.state !== 'TASK_PINNED'),
+        ];
+      }),
       /**
        * Event handler for archiving tasks
        */
