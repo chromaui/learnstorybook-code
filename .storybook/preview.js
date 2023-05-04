c836ffbb1a023af5d818d3cd54dcddbe926ba599
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-05-04 20:51:56 +0100

    Use Pinia and store in Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,12 @@
+import { setup } from '@storybook/vue3';
+import { createPinia } from 'pinia';
+
 import '../src/index.css';
+
+//👇 Registers a global Pinia instance inside Storybook to be consumed by existing stories
+setup((app) => {
+  app.use(createPinia());
+});
 
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
