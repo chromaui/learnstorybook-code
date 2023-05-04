fb37eaabe005819984e1e64e7432552096afbeb2
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-05-04 17:55:38 +0100

    Added stories + default implementation of TaskList

---
+++ src/components/TaskList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="list-items">
+    <template v-if="loading"> loading </template>
+    <template v-else-if="isEmpty"> empty </template>
+    <template v-else>
+      <Task
+        v-for="task in tasks"
+        :key="task.id"
+        :task="task"
+        @archive-task="onArchiveTask"
+        @pin-task="onPinTask"
+      />
+    </template>
+  </div>
+</template>
+
+<script>
+import Task from './Task.vue';
+import { reactive, computed } from 'vue';
+
+export default {
+  name: 'TaskList',
+  components: { Task },
+  props: {
+    tasks: { type: Array, required: true, default: () => [] },
+    loading: { type: Boolean, default: false },
+  },
+  emits: ['archive-task', 'pin-task'],
+
+  setup(props, { emit }) {
+    props = reactive(props);
+    return {
+      isEmpty: computed(() => props.tasks.length === 0),
+      /**
+       * Event handler for archiving tasks
+       */
+      onArchiveTask(taskId) {
+        emit('archive-task', taskId);
+      },
+      /**
+       * Event handler for pinning tasks
+       */
+      onPinTask(taskId) {
+        emit('pin-task', taskId);
+      },
+    };
+  },
+};
+</script>
