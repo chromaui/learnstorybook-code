9bd4b0d6139a30ce849fc55b1ffb93054b2e52f8
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-05-23 18:54:56 +0100

    Added stories + default implementation of TaskList

---
+++ src/app/components/task-list.stories.ts
@@ -0,0 +1,76 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+
+import { componentWrapperDecorator, moduleMetadata } from '@storybook/angular';
+
+import { CommonModule } from '@angular/common';
+
+import TaskListComponent from './task-list.component';
+import TaskComponent from './task.component';
+
+import * as TaskStories from './task.stories';
+
+const meta: Meta<TaskListComponent> = {
+  component: TaskListComponent,
+  title: 'TaskList',
+  tags: ['autodocs'],
+  decorators: [
+    moduleMetadata({
+      //👇 Imports both components to allow component composition with Storybook
+      declarations: [TaskListComponent, TaskComponent],
+      imports: [CommonModule],
+    }),
+    //👇 Wraps our stories with a decorator
+    componentWrapperDecorator(
+      (story) => `<div style="margin: 3em">${story}</div>`
+    ),
+  ],
+  render: (args: TaskListComponent) => ({
+    props: {
+      ...args,
+      onPinTask: TaskStories.actionsData.onPinTask,
+      onArchiveTask: TaskStories.actionsData.onArchiveTask,
+    },
+  }),
+};
+export default meta;
+type Story = StoryObj<TaskListComponent>;
+
+export const Default: Story = {
+  args: {
+    tasks: [
+      { ...TaskStories.Default.args?.task, id: '1', title: 'Task 1' },
+      { ...TaskStories.Default.args?.task, id: '2', title: 'Task 2' },
+      { ...TaskStories.Default.args?.task, id: '3', title: 'Task 3' },
+      { ...TaskStories.Default.args?.task, id: '4', title: 'Task 4' },
+      { ...TaskStories.Default.args?.task, id: '5', title: 'Task 5' },
+      { ...TaskStories.Default.args?.task, id: '6', title: 'Task 6' },
+    ],
+  },
+};
+
+export const WithPinnedTasks: Story = {
+  args: {
+    tasks: [
+      // Shaping the stories through args composition.
+      // Inherited data coming from the Default story.
+      ...(Default.args?.tasks?.slice(0, 5) || []),
+      { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+    ],
+  },
+};
+
+export const Loading: Story = {
+  args: {
+    tasks: [],
+    loading: true,
+  },
+};
+
+export const Empty: Story = {
+  args: {
+    // Shaping the stories through args composition.
+    // Inherited data coming from the Loading story.
+    ...Loading.args,
+    loading: false,
+  },
+};
