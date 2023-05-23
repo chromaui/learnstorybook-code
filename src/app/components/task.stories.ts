594a16bd1fafbb81ca9ff8f8e7b5a2d37c3199e1
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-05-23 18:26:23 +0100

    Added initial task implementation and story

---
+++ src/app/components/task.stories.ts
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+
+import { action } from '@storybook/addon-actions';
+
+import TaskComponent from './task.component';
+
+export const actionsData = {
+  onPinTask: action('onPinTask'),
+  onArchiveTask: action('onArchiveTask'),
+};
+
+const meta: Meta<TaskComponent> = {
+  title: 'Task',
+  component: TaskComponent,
+  excludeStories: /.*Data$/,
+  tags: ['autodocs'],
+  render: (args: TaskComponent) => ({
+    props: {
+      ...args,
+      onPinTask: actionsData.onPinTask,
+      onArchiveTask: actionsData.onArchiveTask,
+    },
+  }),
+};
+
+export default meta;
+type Story = StoryObj<TaskComponent>;
+
+export const Default: Story = {
+  args: {
+    task: {
+      id: '1',
+      title: 'Test Task',
+      state: 'TASK_INBOX',
+    },
+  },
+};
+
+export const Pinned: Story = {
+  args: {
+    task: {
+      ...Default.args?.task,
+      state: 'TASK_PINNED',
+    },
+  },
+};
+
+export const Archived: Story = {
+  args: {
+    task: {
+      ...Default.args?.task,
+      state: 'TASK_ARCHIVED',
+    },
+  },
+};
