33a3c96f8f468d9bd0432b77af2307798fd7efc5
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-11-02 18:19:56 +0000

    Added initial task implementation and story

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,9 @@
+import '../src/index.css';
+
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
---
+++ src/components/Task.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="list-item">
+    <label for="title" :aria-label="task.title">
+      <input type="text" readonly :value="task.title" id="title" name="title" />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Task',
+  props: {
+    task: {
+      type: Object,
+      required: true,
+      default: () => ({ id: '', state: '', title: '' }),
+      validator: (task) => ['id', 'state', 'title'].every((key) => key in task),
+    },
+  },
+};
+</script>
