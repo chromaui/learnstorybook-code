5deb9f70d3adc1e5918117b744dbf00c57d3c394
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-11-02 18:04:15 +0000

    Add and integrate controls story

--- src/components/Task.stories.jsx
+++ src/components/Task.stories.jsx
@@ -33,3 +33,14 @@ export const Archived = {
     },
   },
 };
+
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
+export const LongTitle = {
+  args: {
+    task: {
+      ...Default.args.task,
+      title: longTitleString,
+    },
+  },
+};
