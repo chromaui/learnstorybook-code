40e6ea69f4e5a377514f8fb7500e04f6b64ef832
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-11-02 18:42:38 +0000

    Added InboxScreen component

---
+++ src/components/PureInboxScreen.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-if="error" class="page lists-show">
+      <div class="wrapper-message">
+        <span class="icon-face-sad" />
+        <p class="title-message">Oh no!</p>
+        <p class="subtitle-message">Something went wrong</p>
+      </div>
+    </div>
+    <div v-else class="page lists-show">
+      <nav>
+        <h1 class="title-page">Taskbox</h1>
+      </nav>
+      <TaskList />
+    </div>
+  </div>
+</template>
+
+<script>
+import TaskList from './TaskList.vue';
+export default {
+  name: 'PureInboxScreen',
+  components: { TaskList },
+  props: {
+    error: { type: Boolean, default: false },
+  },
+};
+</script>
