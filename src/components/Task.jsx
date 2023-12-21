f89cfe08583fba4dcc00680b81a4968dc4e3375e
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-12-21 22:14:15 +0000

    Add and integrate controls story

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -30,6 +30,7 @@ export default function Task({
           readOnly={true}
           name="title"
           placeholder="Input title"
+          style={{ textOverflow: "ellipsis" }}
         />
       </label>
 
