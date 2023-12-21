d52ad141f92d0ec457352c9e5a3100c49b7db4dc
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-12-21 22:08:37 +0000

    Added Interactive Story

--- src/components/InboxScreen.stories.jsx
+++ src/components/InboxScreen.stories.jsx
@@ -4,6 +4,13 @@ import { rest } from "msw";
 import { MockedState } from "./TaskList.stories";
 import { Provider } from "react-redux";
 
+import {
+  fireEvent,
+  waitFor,
+  within,
+  waitForElementToBeRemoved,
+} from "@storybook/test";
+
 export default {
   component: InboxScreen,
   title: "InboxScreen",
@@ -23,6 +30,18 @@ export const Default = {
         ),
       ],
     },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    // Waits for the component to transition from the loading state
+    await waitForElementToBeRemoved(await canvas.findByTestId("loading"));
+    // Waits for the component to be updated based on the store
+    await waitFor(async () => {
+      // Simulates pinning the first task
+      await fireEvent.click(canvas.getByLabelText("pinTask-1"));
+      // Simulates pinning the third task
+      await fireEvent.click(canvas.getByLabelText("pinTask-3"));
+    });
   },
 };
 export const Error = {
