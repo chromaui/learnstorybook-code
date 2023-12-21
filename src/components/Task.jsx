8c31853ea88e5556c7d6d7e97a5941fdf0517b66
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-12-21 21:27:16 +0000

    Added propTypes to Task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export default function Task({
   task: { id, title, state },
@@ -46,3 +47,19 @@ export default function Task({
     </div>
   );
 }
+
+Task.propTypes = {
+  /** Composition of the task */
+  task: PropTypes.shape({
+    /** Id of the task */
+    id: PropTypes.string.isRequired,
+    /** Title of the task */
+    title: PropTypes.string.isRequired,
+    /** Current state of the task */
+    state: PropTypes.string.isRequired,
+  }),
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
+};
