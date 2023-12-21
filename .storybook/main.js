9b36e1a00f72fba8ccb758e74446952139841391
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-12-21 21:28:54 +0000

    Added A11y addon and fixed issues

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,7 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-vite",
