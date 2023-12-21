42f639fa97da8f57db6a5f8d03a4c4d3e1440f3c
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-12-21 22:52:01 +0000

    Added InboxScreen component

---
+++ src/components/PureInboxScreen.stories.js
@@ -0,0 +1,13 @@
+import PureInboxScreen from './PureInboxScreen.vue';
+
+export default {
+  component: PureInboxScreen,
+  title: 'PureInboxScreen',
+  tags: ['autodocs'],
+};
+
+export const Default = {};
+
+export const Error = {
+  args: { error: true },
+};
