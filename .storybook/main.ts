6f01fae2ddfc6e5da165feef3ab0350c4ea1cd71
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-12-22 20:56:08 +0000

    Added initial task implementation and story

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,17 @@
-import type { StorybookConfig } from "@storybook/angular";
+import type { StorybookConfig } from '@storybook/angular';
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/app/components/**/*.stories.ts'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/angular",
+    name: '@storybook/angular',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
   },
 };
 export default config;
---
+++ src/app/components/task.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  template: `
+    <div class="list-item">
+      <label [attr.aria-label]="task.title + ''" for="title">
+        <input
+          type="text"
+          [value]="task.title"
+          readonly="true"
+          id="title"
+          name="title"
+        />
+      </label>
+    </div>
+  `,
+})
+export default class TaskComponent {
+  /**
+   * The shape of the task object
+   */
+  @Input() task: any;
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onPinTask = new EventEmitter<Event>();
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onArchiveTask = new EventEmitter<Event>();
+}
