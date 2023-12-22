b4ebe43806f4370bf165372d6e0c672702290859
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-12-22 21:01:30 +0000

    Added A11y addon and fixed issues

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,7 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/angular',
