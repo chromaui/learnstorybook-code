685930a50acb84c058ba5a32dc0caa16794ef385
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-12-22 20:59:20 +0000

    Implemented more complex task

--- src/app/components/task.component.ts
+++ src/app/components/task.component.ts
@@ -1,18 +1,46 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../models/task.model';
 
 @Component({
   selector: 'app-task',
   template: `
-    <div class="list-item">
-      <label [attr.aria-label]="task.title + ''" for="title">
+    <div class="list-item {{ task?.state }}">
+      <label
+        [attr.aria-label]="'archiveTask-' + task.id"
+        for="checked-{{ task?.id }}"
+        class="checkbox"
+      >
+        <input
+          type="checkbox"
+          disabled="true"
+          [defaultChecked]="task?.state === 'TASK_ARCHIVED'"
+          name="checked-{{ task?.id }}"
+          id="checked-{{ task?.id }}"
+        />
+        <span class="checkbox-custom" (click)="onArchive(task.id)"></span>
+      </label>
+      <label
+        [attr.aria-label]="task.title + ''"
+        for="title-{{ task?.id }}"
+        class="title"
+      >
         <input
           type="text"
           [value]="task.title"
           readonly="true"
-          id="title"
-          name="title"
+          id="title-{{ task?.id }}"
+          name="title-{{ task?.id }}"
+          placeholder="Input title"
         />
       </label>
+      <button
+        *ngIf="task?.state !== 'TASK_ARCHIVED'"
+        class="pin-button"
+        [attr.aria-label]="'pinTask-' + task.id"
+        (click)="onPin(task.id)"
+      >
+        <span class="icon-star"></span>
+      </button>
     </div>
   `,
 })
@@ -20,7 +48,7 @@ export default class TaskComponent {
   /**
    * The shape of the task object
    */
-  @Input() task: any;
+  @Input() task?: Task;
 
   // tslint:disable-next-line: no-output-on-prefix
   @Output()
@@ -29,4 +57,21 @@ export default class TaskComponent {
   // tslint:disable-next-line: no-output-on-prefix
   @Output()
   onArchiveTask = new EventEmitter<Event>();
+
+  /**
+   * @ignore
+   * Component method to trigger the onPin event
+   * @param id string
+   */
+  onPin(id: any) {
+    this.onPinTask.emit(id);
+  }
+  /**
+   * @ignore
+   * Component method to trigger the onArchive event
+   * @param id string
+   */
+  onArchive(id: any) {
+    this.onArchiveTask.emit(id);
+  }
 }
