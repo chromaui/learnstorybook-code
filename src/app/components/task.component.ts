f6d3d1c83854a02fdc991753a082fce5fbe8cc7c
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2025-01-10 21:26:47 +0000

    Add and integrate controls story

--- src/app/components/task.component.ts
+++ src/app/components/task.component.ts
@@ -32,6 +32,7 @@ import { Task } from '../models/task.model';
           id="title-{{ task?.id }}"
           name="title-{{ task?.id }}"
           placeholder="Input title"
+          style="text-overflow: ellipsis;"
         />
       </label>
       <button
