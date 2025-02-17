cefd7a0bd760801e521676a6b93c46368a87d2a7
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2025-02-17 15:47:21 +0000

    Implemented more complex task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -5,8 +5,11 @@ type TaskData = {
 };
 
 type TaskProps = {
+  /** Composition of the task */
   task: TaskData;
+  /** Event to change the task to archived */
   onArchiveTask: (id: string) => void;
+  /** Event to change the task to pinned */
   onPinTask: (id: string) => void;
 };
 
@@ -16,16 +19,43 @@ export default function Task({
   onPinTask,
 }: TaskProps) {
   return (
-    <div className="list-item">
-      <label htmlFor={`title-${id}`} aria-label={title}>
+    <div className={`list-item ${state}`}>
+      <label
+        htmlFor={`archiveTask-${id}`}
+        aria-label={`archiveTask-${id}`}
+        className="checkbox"
+      >
+        <input
+          type="checkbox"
+          disabled={true}
+          name="checked"
+          id={`archiveTask-${id}`}
+          checked={state === "TASK_ARCHIVED"}
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+      </label>
+
+      <label htmlFor={`title-${id}`} aria-label={title} className="title">
         <input
           type="text"
           value={title}
           readOnly={true}
           name="title"
           id={`title-${id}`}
+          placeholder="Input title"
         />
       </label>
+      {state !== "TASK_ARCHIVED" && (
+        <button
+          className="pin-button"
+          onClick={() => onPinTask(id)}
+          id={`pinTask-${id}`}
+          aria-label={`pinTask-${id}`}
+          key={`pinTask-${id}`}
+        >
+          <span className={`icon-star`} />
+        </button>
+      )}
     </div>
   );
 }
