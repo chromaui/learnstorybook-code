bc897c55605cbb30b4f4550a99172196d43c0dbd
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2025-02-17 15:51:42 +0000

    Added A11y addon and fixed issues

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,6 +7,7 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
   ],
   framework: {
     name: "@storybook/react-vite",
