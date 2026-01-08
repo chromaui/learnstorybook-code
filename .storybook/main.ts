63ec4520bbf7ce46082df0e1f7d9f4fe249e85c0
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2026-01-08 19:57:51 +0000

    Added A11y addon and fixed issues

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: ['../src/app/components/**/*.stories.ts'],
-  addons: ['@storybook/addon-docs'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-a11y'],
   staticDirs: ['../public'],
   framework: {
     name: '@storybook/angular',
