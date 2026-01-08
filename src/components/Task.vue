b798a509af47224e1f835bec89d001a398e4363b
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2026-01-08 17:24:20 +0000

    Implemented more complex task

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,12 +1,40 @@
 <template>
-  <div class="list-item">
-    <label for="title" :aria-label="task.title">
-      <input type="text" readonly :value="task.title" id="title" name="title" />
+  <div :class="classes">
+    <label :for="'checked' + task.id" :aria-label="'archiveTask-' + task.id" class="checkbox">
+      <input
+        type="checkbox"
+        :checked="isChecked"
+        disabled
+        :name="'checked' + task.id"
+        :id="'archiveTask-' + task.id"
+      />
+      <span class="checkbox-custom" @click="archiveTask" />
     </label>
+    <label :for="'title-' + task.id" :aria-label="task.title" class="title">
+      <input
+        type="text"
+        readonly
+        :value="task.title"
+        :id="'title-' + task.id"
+        name="title"
+        placeholder="Input title"
+      />
+    </label>
+    <button
+      v-if="!isChecked"
+      class="pin-button"
+      @click="pinTask"
+      :id="'pinTask-' + task.id"
+      :aria-label="'pinTask-' + task.id"
+    >
+      <span class="icon-star" />
+    </button>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { computed } from 'vue'
+
 type TaskData = {
   id: string
   title: string
@@ -14,11 +42,43 @@ type TaskData = {
 }
 
 type TaskProps = {
+  /** Composition of the task */
   task: TaskData
+  /** Event to change the task to archived */
   onArchiveTask: (id: string) => void
+  /** Event to change the task to pinned */
   onPinTask: (id: string) => void
 }
+
 const props = withDefaults(defineProps<TaskProps>(), {
   task: { id: '', title: '', state: 'TASK_INBOX' },
 })
+
+const classes = computed(() => {
+  return `list-item ${props.task.state}`
+})
+
+/*
+ * Computed property for checking the state of the task
+ */
+const isChecked = computed(() => props.task.state === 'TASK_ARCHIVED')
+
+const emit = defineEmits<{
+  (e: 'archive-task', id: string): void
+  (e: 'pin-task', id: string): void
+}>()
+
+/**
+ * Event handler for archiving tasks
+ */
+function archiveTask() {
+  emit('archive-task', props.task.id)
+}
+
+/**
+ * Event handler for pinning tasks
+ */
+function pinTask(): void {
+  emit('pin-task', props.task.id)
+}
 </script>
