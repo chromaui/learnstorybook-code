c73e1749dddd2e9699e72f066573707197584189
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2026-01-08 17:45:53 +0000

    Added A11y addon and fixed issues

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,12 @@ import type { StorybookConfig } from '@storybook/vue3-vite'
 const config: StorybookConfig = {
   stories: ['../src/components/**/*.stories.ts'],
   staticDirs: ['../public'],
-  addons: ['@chromatic-com/storybook', '@storybook/addon-docs', '@storybook/addon-vitest'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs',
+    '@storybook/addon-vitest',
+    '@storybook/addon-a11y',
+  ],
   framework: {
     name: '@storybook/vue3-vite',
     options: {},
